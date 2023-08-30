cb72c57e740a2b48e66888c62c6fb8bbde9ecd8a
Paulus H.J. Schulinck <PaulskPt@users.noreply.github.com>
2023-08-30 21:04:48 +0100

    Update boot_pico.py
    Alternative added as example

--- Boards/raspberrypi/Raspberry Pi Pico/TR Cowbell/v1.2_mods_by_PaulskPt/boot_pico.py
+++ Boards/raspberrypi/Raspberry Pi Pico/TR Cowbell/v1.2_mods_by_PaulskPt/boot_pico.py
@@ -6,4 +6,5 @@ up_btn = digitalio.DigitalInOut(board.GP21)
 up_btn.direction = digitalio.Direction.INPUT # BUTTON 1
 up_btn.pull = digitalio.Pull.UP
 
-storage.remount("/", not up_btn.value)
+storage.remount("/", not up_btn.value)   
+# storage.remount("/", False) # If you don't want to press the UP-button (BUTTON 1) at boot, use this line
